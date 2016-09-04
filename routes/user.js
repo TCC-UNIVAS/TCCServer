e15ab99d24683441cd83195fd9a7d9efc9880d85
Daniela Mara <danielamaradms@gmail.com>
2016-09-04 18:16:23 -0300

    Enviando o status da requisição

--- routes/user.js
+++ routes/user.js
@@ -29,12 +29,14 @@ router.post('/token', function(req, res){
     var sendData = function(affectedRows){
         if(affectedRows){
             res.status(200).json({
-                message : 'token registred with success'
+                message: 'token registred with success',
+                status: 200
             });
         }
         else{
             res.status(400).json({
-                message : 'An error occour when set the token, try later'
+                message: 'An error occour when set the token, try later',
+                status: 400
             });
         }
     }
@@ -44,15 +46,15 @@ router.post('/token', function(req, res){
 
 //get a 'user' json with data to insert in db
 //before insert it's necessary verify if the email exists in db, so the middleware is called to check the email
-router.post('/', user_middleware.checkEmail, function(req, res){    
+router.post('/', user_middleware.checkEmail, function(req, res){
          user_service.addUser(req);
          res.status(200).json({
              massege : "User added"
-         });   
-    }        
+         });
+    }
 );
 
-//make the user authentication, return all data of user if the username e password are corect. 
+//make the user authentication, return all data of user if the username e password are corect.
 router.post('/auth', function(req, res){
     var sendData = function(auth){
         if(auth)
@@ -63,7 +65,7 @@ router.post('/auth', function(req, res){
             });
     };
 
-    user_service.auth(req.body.email, req.body.password, sendData);    
+    user_service.auth(req.body.email, req.body.password, sendData);
 });
 
 
@@ -78,12 +80,12 @@ router.get('/push', function (req, res) {
     message.collapseKey = 'Testing Push'; //grouping messages
     message.delayWhileIdle = true; //delay sending while receiving device is offline
     message.timeToLive = 999999; //number of seconds to keep the message on server if the device is offline
-    
-    //Take the registration id(lengthy string) that you logged 
+
+    //Take the registration id(lengthy string) that you logged
     //in your ionic v2 app and update device_tokens[0] with it for testing.
-    //Later save device tokens to db and 
+    //Later save device tokens to db and
     //get back all tokens and push to multiple devices
-    
+
     sender.send(message, device_tokens, retry_times, function (result) {
         console.log('push sent to: ' + device_tokens);
         res.status(200).send('Pushed notification ' + device_tokens);
