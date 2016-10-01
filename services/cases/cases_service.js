88d56a1a5599d4fbedd460efccbeba1e3aec84b1
Daniela Mara <danielamaradms@gmail.com>
2016-10-01 03:03:51 -0300

    Alter query cases last week

--- services/cases/cases_service.js
+++ services/cases/cases_service.js
@@ -3,16 +3,14 @@ var db = require('../../util/db_conn');
 
 var getCasesFromLastWeekByUserId = function(lat, lng, userId, callback) {
     db.query('SELECT ' +
-            'c.case_id, c.user_id, c.category_id, c.lat, c.lng' +
-        	', c.comments, DATE_FORMAT(c.create_date, "%d %b %Y %T:%f")' +
-        	', c.image, c.address, c.neighborhood' +
+            'c.*, DATE_FORMAT(c.create_date,"%d %b %Y %T:%f") AS formated_date' +
         	', case c.category_id' +
                 'when 1 then "Suspeita de focos do mosquito Aedes aegypti" ' +
                 'when 2 then "Suspeita de Dengue" ' +
         		'when 3 then "Suspeita de Zica" ' +
         		'when 4 then "Supeita de Chikungunya" ' +
-            'end as category_description' +
-        	', ct.name as category_name' +
+            'end AS category_description' +
+        	', ct.name AS category_name' +
             'FROM cases c ' +
             'JOIN category ct ON c.category_id = ct.category_id ' +
             'WHERE ((( ACOS( COS( ( 90 - ? ) * PI() / 180 ) * ' +
